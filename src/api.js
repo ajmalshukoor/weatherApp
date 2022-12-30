8b410eb1766969274c243ffa5fefaba7a781046d
ajmalshukoor <ajmalshukoor11@gmail.com>
2022-12-29 20:33:22 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a82cdaaad2msh735b65e6bc4d99dp1f8a3ejsn48635fc70e74',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "3fe05fe37f4993bdca04d6ae6633872f";
+// export const WEATHER_API_KEY = "";
